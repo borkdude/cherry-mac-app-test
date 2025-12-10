d711ca84bea100c3810f1a7fd54d75642631afee
Michiel Borkent <michielborkent@gmail.com>
2025-12-10 14:46:36 +0100

    console.log

--- runJS.swift
+++ runJS.swift
@@ -4,6 +4,24 @@ import JavaScriptCore
 // Create a JS context
 let context = JSContext()!
 
+extension JSContext {
+    func injectConsoleLog() {
+
+        evaluateScript(
+          """
+            var console = {};
+            """
+        )
+        let consoleLog: @convention(block) (Any) -> Void = {
+            print($0)
+        }
+        objectForKeyedSubscript("console")
+          .setObject(consoleLog, forKeyedSubscript: "log" as NSString)
+    }
+}
+
+context.injectConsoleLog()
+
 // Optional: handle JS exceptions
 context.exceptionHandler = { context, exception in
     print("JS Error: \(exception!)")
